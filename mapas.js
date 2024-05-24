e027f903bbf3932ce2ea32418c8ffd35cfd34e78
Cecilia <ceciferre@gmail.com>
2024-05-24 20:49:54 -0300

    Haciendo mejoras

--- mapas.js
+++ mapas.js
@@ -1,17 +1,17 @@
 //Funcion para el mapa
-function initMap() {
+function initMap() { 
     var mapOptions = {
-        center: { lat: -34.603722, lng: -58.381592 },
+        center: { lat: -34.589299601070934, lng: -58.41088279749599 },
         zoom: 20
     };
 
     var map1 = new google.maps.Map(document.getElementById('map1'), mapOptions);
     var map2 = new google.maps.Map(document.getElementById('map2'), mapOptions);
     var map3 = new google.maps.Map(document.getElementById('map3'), mapOptions);
-
-    new google.maps.Marker({ position: { lat: -34.603722, lng: -58.381592 }, map: map1 });
-    new google.maps.Marker({ position: { lat: -34.603722, lng: -58.381592 }, map: map2 });
-    new google.maps.Marker({ position: { lat: -34.603722, lng: -58.381592 }, map: map3 });
+    
+    new google.maps.Marker({ position: { lat:  -34.589299601070934, lng:  -58.41088279749599}, map: map1 });
+    new google.maps.Marker({ position: { lat: -34.589299601070934, lng: -58.41088279749599 }, map: map2 });
+    new google.maps.Marker({ position: { lat: -34.589299601070934, lng: -58.41088279749599 }, map: map3 });
 }
 
 window.initMap = initMap;
